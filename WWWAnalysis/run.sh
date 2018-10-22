b01daabf11e890c4cede538aa5af326fe468efd7
Philip Chang <sgnoohc@gmail.com>
2018-10-21 23:32:52 -0700

    update

--- WWWAnalysis/run.sh
+++ WWWAnalysis/run.sh
@@ -1,6 +1,6 @@
 for f in $(ls /nfs-7/userdata/phchang/WWW_babies/WWW2017_v4.0.2/skim/*.root); do
     echo "Launched a job for $f"
-    ./doAnalysis $f outputs/$(basename $f) t 50000 > outputs/$(basename $f).log 2>&1 &
+    ./doAnalysis $f t outputs/$(basename $f) -1 > outputs/$(basename $f).log 2>&1 &
 done
 
 wait
